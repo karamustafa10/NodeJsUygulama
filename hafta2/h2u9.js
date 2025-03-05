5e3536950338ca4a2b0531d5dd73257082993e7e
karamustafa10 <semih_karamustafa@hotmail.com>
2025-03-05 09:38:41 +0300

    Add book object and log book details to console

---
+++ hafta2/h2u9.js
@@ -0,0 +1,15 @@
+const book = {
+  title: "1984",
+  author: "George Orwell",
+  pageCount: 326,
+};
+
+console.log(
+  "The book is called " +
+    book.title +
+    " and it was written by " +
+    book.author +
+    " and it has " +
+    book.pageCount +
+    " pages."
+);
